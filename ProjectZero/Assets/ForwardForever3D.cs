bafd7223047ce1cd78120c21095257944d71715d
MRMJR2006 <38221736+MRMJR2006@users.noreply.github.com>
2019-06-06 20:13:07 +0100

    Vastly Improved Player Movement
    Vastly improved player movement with help of scripts imported yesterday.  IMPORTANT NOTE: Player_Movement.cs was deleted because it was no longer required.  Other script is likely to stay in project

--- ProjectZero/Assets/ForwardForever3D.cs
+++ ProjectZero/Assets/ForwardForever3D.cs
@@ -7,37 +7,25 @@ using UnityEngine;
 public class ForwardForever3D : MonoBehaviour
 {
     public Rigidbody rb;
-    public Transform player;
-    public float forwardSpeed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //checking to see if player has NOT got a value asigned yet
-        if (player != null)
-        {
-            //setting it equal to the Transform on this game object
-            player = GetComponent<Transform>();
-        }
-        //checking to see if rb has NOT got a value asigned yet
-        if (rb != null)
-        {
-            //setting it equal to the Rigidbody on this game object
-            rb = GetComponent<Rigidbody>();
-        }
-    }
-
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-    //called every physics update
-    private void FixedUpdate()
-    {
-        Vector3 velocity = (forwardSpeed, 0f, 0f) * Time.deltaTime;
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
 
-        rb.MovePosition(velocity);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
     }
 }
