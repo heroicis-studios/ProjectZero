dc68ec517abac38be8e97d4181346858832c3302
MRMJR2006 <38221736+MRMJR2006@users.noreply.github.com>
2019-07-17 19:13:43 +0100

    ORGANISATION
    -Organisation
    
    IMPORTANT: Obstacles aren't working at the moment

---
+++ ProjectZero/Assets/Scripts/Player In-Game Controls/PlayerDie.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//If a Transform is not present on the object, create one
+[RequireComponent(typeof(Transform))]
+public class PlayerDie : MonoBehaviour
+{
+    public float deathHeight = -10f;
+
+    Transform trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+
+
+    //called after the Update function
+    private void LateUpdate()
+    {
+        //If the transform's y position is less than or equal to the deathHeight, 'Die'
+        if (trans.position.y <= deathHeight)
+        {
+            Die();
+        }
+    }
+
+    //placed into function as called from multiple places
+    public void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
---
+++ ProjectZero/Assets/Scripts/PlayerObstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObstacle : MonoBehaviour
+{
+
+    public PlayerDie die;
+    private Transform player;
+
+    //when an object collides with something, this is called
+    private void OnCollisionEnter(Collision collision)
+    {
+        //checking the 'layer' that the object we collided with is on
+        if (collision.collider.gameObject.layer == 9) {
+            die.Die();
+        }
+    }
+}
