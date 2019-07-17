484148df5a0099fb198ca27482718d7ac7dadaae
MRMJR2006 <38221736+MRMJR2006@users.noreply.github.com>
2019-07-17 18:24:01 +0100

    Sideways Movement System and Computer Edited Files
    A sideways movement with a constant force: no force or xx force
    
    Overall, very happy

--- ProjectZero/Assets/Scripts/Player In-Game Controls/ForwardForever3D.cs
+++ ProjectZero/Assets/Scripts/Player In-Game Controls/ForwardForever3D.cs
@@ -17,28 +17,34 @@ public class ForwardForever3D : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float VelocityX = 0f;
+        VelocityX = 0f;
+
         //applying forward force every frame multiplied by time since last frame
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         //if d or right arrow is pressed, apply a sideways force to the right
-        if (Input.GetKeyDown("d") || Input.GetKeyDown("right"))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
-
+            VelocityX = sidewaysForce * Time.deltaTime;
         }
 
         //if a or left arrow is pressed, apply a sideways force to the left
-        if (Input.GetKeyDown("a") || Input.GetKeyDown("left"))
+        if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            VelocityX = -sidewaysForce * Time.deltaTime;
 
         }
 
         //if space, w or up arrow is pressed, call function jump
-        if (Input.GetKeyDown("w") || Input.GetKeyDown("up") || Input.GetKeyDown("space"))
+        if (Input.GetKey("w") || Input.GetKey("up") || Input.GetKey("space"))
         {
             Jump();
         }
+
+        float VelocityY = rb.velocity.y;
+        Vector3 Velocity = new Vector3(VelocityX, VelocityY, forwardForce * Time.deltaTime);
+        rb.velocity = Velocity;
     }
 
     public void Jump()
