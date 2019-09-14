d174979583cb8cf1853356ece1bb5dfd59b6009a
MRMJR2006 <38221736+MRMJR2006@users.noreply.github.com>
2019-09-14 11:29:54 +0100

    Player Speed Up Implemented
    -Player Speed Up now works
    -Foundtion for future mini-modes
    -Removal of the Protocol scripts and objects

---
+++ ProjectZero/Assets/PlayerTriggerF3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTriggerF3D : MonoBehaviour
+{
+    private ForwardForever3D f3d;
+
+    private void Start()
+    {
+        f3d = GetComponent<ForwardForever3D>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerSpeedUp>() != null)
+        {
+            PlayerSpeedUp SpeedUp = other.GetComponent<PlayerSpeedUp>();
+            f3d.speedMultiplier += SpeedUp.playerSpeedChange;
+        }
+    }
+}
